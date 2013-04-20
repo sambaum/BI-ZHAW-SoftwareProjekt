7ab10b3edaa8fd4f5fdce20e2dc8d284ac9ab188
sambaum <samuel.baumgartner@gmail.com>
2013-04-20 12:31:34 +0200

    Erstellen der ersten Klassen

---
+++ MultiChannel/src/User.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Hier kommt unsere Doku für diese Klasse hin.
+ * @author Samuel
+ *
+ */
+
+public class User {
+	private String userName;
+	private String group;
+	private String tel;
+	private String email;
+	private String druckerName;
+	private ArrayList<SMS> smsBox;
+	private ArrayList<Mail> mailBox;
+	private ArrayList<Print> printBox;
+}
