055d9353694a14d1af46913161d49d353808fd62
sambaum <samuel.baumgartner@gmail.com>
2013-04-23 22:15:11 +0200

    getter und setter methoden für Klasse User

--- MultiChannel/src/User.java
+++ MultiChannel/src/User.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 
 /**
- * Hier kommt unsere Doku für diese Klasse hin.
+ * Hier kommt unsere Doku und Beschreibung für diese Klasse hin.
  * @author Samuel
  * 
  */
 
 public class User {
+	
+	// Instanzvariablen
 	private String userName;
 	private String group;
 	private String tel;
@@ -15,4 +17,55 @@ public class User {
 	private ArrayList<SMS> smsBox;
 	private ArrayList<Mail> mailBox;
 	private ArrayList<Print> printBox;
+	
+	// getter & setter section
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPrinterName() {
+		return printerName;
+	}
+	public void setPrinterName(String printerName) {
+		this.printerName = printerName;
+	}
+	public ArrayList<SMS> getSmsBox() {
+		return smsBox;
+	}
+	public void setSmsBox(ArrayList<SMS> smsBox) {
+		this.smsBox = smsBox;
+	}
+	public ArrayList<Mail> getMailBox() {
+		return mailBox;
+	}
+	public void setMailBox(ArrayList<Mail> mailBox) {
+		this.mailBox = mailBox;
+	}
+	public ArrayList<Print> getPrintBox() {
+		return printBox;
+	}
+	public void setPrintBox(ArrayList<Print> printBox) {
+		this.printBox = printBox;
+	}
 }
