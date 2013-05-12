29fa67f045c0832737a000e3331780d41b08061a
KiddieLancer <dj.bmc@gmx.ch>
2013-05-12 21:02:29 +0200

    Datum auf Gültigkeit prüfen

--- MultiChannel/src/CheckDate.java
+++ MultiChannel/src/CheckDate.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * Diese Klasse überprüft ob ein String ein Datum ist.
  * @author Samuel
@@ -9,7 +12,21 @@ public class CheckDate implements Check{
 	@Override
 	public boolean check(String stringToCheck) {
 		// TODO Auto-generated method stub
-		return false;
+		return isDate(stringToCheck);
+	}
+
+	private boolean isDate(String stringToCheck) {
+		// TODO Auto-generated method stub
+		try {
+		      //Check date validation
+		      SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		      dateFormat.setLenient(false);
+		      dateFormat.parse(stringToCheck);
+		      return true;
+		}
+		catch (ParseException e) {
+		      return false;
+		}
 	}
 	
 }
