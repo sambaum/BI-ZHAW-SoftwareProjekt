b4c32137539075d6a4f1f9923fc04446dffafc08
sambaum <samuel.baumgartner@gmail.com>
2013-06-02 23:15:19 +0200

    createUser() in AdminSession

--- MultiChannel/src/AdminSession.java
+++ MultiChannel/src/AdminSession.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.HashMap;
 
 
@@ -13,7 +14,30 @@ public class AdminSession extends MenuBasedClasses{
 		HashMap<String, String> initMenu = new HashMap<String, String>();
 		initMenu.put("1", "User erfassen");
 		initMenu.put("2", "User löschen");
-		//initMenu.put("3", "User bearbeiten");
 		String antwort = askAndGetAnswerWithList(initMenu, "Was möchten Sie tun?:");
+		if (antwort.equals("1")){
+			createUser();
+		} else if(antwort.equals("2")){
+			deleteuser();
+		}
+	}
+
+	private void deleteuser() {
+		// TODO Auto-generated method stub
+	}
+
+	private void createUser() {
+		System.out.println("Ein neuer User wird erfasst...");
+		String username = askAndGetAnswer("Usernamen:");
+		String group = askAndGetAnswer("Gruppe:");
+		String tel = askAndGetAnswer("Telefon-Nummer:");
+		String email = askAndGetAnswer("E-Mail:");
+		String printer = askAndGetAnswer("Drucker-Namen");
+		try {
+			getStoreuser().write(new User(username, group, tel, email, printer));
+		} catch (IOException e) {
+			System.out.println("Bei speichern des Users ist ein Fehler aufgetreten");
+			e.printStackTrace();
+		}
 	}
 }
